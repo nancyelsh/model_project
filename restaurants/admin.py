3a4e8ff2c83570861508eb805f086593deef8acc
Nancy El-Sharkawi <51517316+nancyelsh@users.noreply.github.com>
2023-02-03 21:33:58 +0300

    finished project - this is model project not admin

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Restaurant
 
+class RstaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    list_filter = ('open', )
+    search_fields = ('name', )
+
+
+
 # Register your models here.
-admin.site.register(Restaurant)
+admin.site.register(Restaurant, RstaurantAdmin)
--- restaurants/models.py
+++ restaurants/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Restaurant(models.Model):
 
     name = models.CharField(max_length=30)
-    description = models.CharField(default="")
+    description = models.CharField(max_length=250, default="")
     opening_time = models.TimeField(auto_now=False, auto_now_add=False)
     closing_time = models.TimeField(auto_now=False, auto_now_add=False)
+    open = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} {self.description} {self.opening_time} {self.closing_time} {self.created_at}"
